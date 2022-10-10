6f621ee9dea0dfbd31b02dec899d9751949bb9b4
James Owiti Maurice <james.owiti@student.moringaschool.com>
2022-10-10 19:32:01 +0300

    Mmanaged to display first containing functions

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {useState} from 'react';
-/*import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCircleCheck, faPen, faTrash, faPlus
-} from '@fortawesome/free-solid-svg-icons';*/
+} from '@fortawesome/free-solid-svg-icons';
+
+
 function App() {
 
-const [todo, setTodo] = useState([
-  {"id": 1, "title": "Learn React", "status": false},
-  {"id": 2, "title": "Learn Angular", "status": false},
-]);
+  const [toDo, setTodo] = useState([
+    { "id": 1, "title": "Learn React", "status": false },
+    { "id": 2, "title": "Learn Angular", "status": false }
+  ]);
 
-//temporarily store the task
-const [newTask, setNewTask] = useState("");
-const [updateData, setUpdateData = useState("");
+  //temporarily store the task
+  const [newTask, setNewTask] = useState('');
+  const [updateData, setUpdateData] = useState(''); //for updating the task
 
-//add new task
-const addTask = (e) => {
-}
 
-//Delete task
-const deleteTask = (id) => {
-}
 
-//Mark task as done
-const markDone = (id) => {
-}
 
-//Cancel update
-const cancelUpdate = () => {
-}
+  //add new task
+  const addTask = (e) => {
+  }
 
-//Change the task
-const changeTask = (e) => {
-}
+  //Delete task
+  const deleteTask = (id) => {
+  }
 
-//Update task
-const updateTask = (id) => {
-}
+  //Mark task as done
+  const markDone = (id) => {
+  }
+
+  //Cancel update
+  const cancelUpdate = () => {
+  }
+
+  //Change the task
+  const changeTask = (e) => {
+  }
+
+  //Update task
+  const updateTask = (id) => {
+  }
 
 
   return (
@@ -46,6 +51,20 @@ const updateTask = (id) => {
       <br></br>
       <h2>(James' To Do List App)</h2>
       <br></br>
+
+      {/*Dispaly To */}
+
+      {toDo && toDo.length ? '' : 'No tasks to do'}
+
+      {toDo && toDo
+      .map((task, index) => {
+      return(
+      <React.Fragment key={task.id}>
+        <span className="taskText">{task.title}</span>
+      </React.Fragment>
+      )
+})
+    }
 
     </div>
   );
