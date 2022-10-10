809013ae0d81d644b65d12ef20636ebe398d3d5b
James Owiti Maurice <james.owiti@student.moringaschool.com>
2022-10-10 18:21:47 +0300

    Made all functions in App.js for ease of creating functions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {useState} from 'react';
+/*import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {
   faCircleCheck, faPen, faTrash, faPlus
-} from '@fortawesome/free-solid-svg-icons';
+} from '@fortawesome/free-solid-svg-icons';*/
 function App() {
+
+const [todo, setTodo] = useState([
+  {"id": 1, "title": "Learn React", "status": false},
+  {"id": 2, "title": "Learn Angular", "status": false},
+]);
+
+//temporarily store the task
+const [newTask, setNewTask] = useState("");
+const [updateData, setUpdateData = useState("");
+
+//add new task
+const addTask = (e) => {
+}
+
+//Delete task
+const deleteTask = (id) => {
+}
+
+//Mark task as done
+const markDone = (id) => {
+}
+
+//Cancel update
+const cancelUpdate = () => {
+}
+
+//Change the task
+const changeTask = (e) => {
+}
+
+//Update task
+const updateTask = (id) => {
+}
+
+
   return (
     <div className="container App">
       <br></br>
       <h2>(James' To Do List App)</h2>
       <br></br>
-      
+
     </div>
   );
 }
--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -1,11 +1,1 @@
 import React from 'react';
-
-const AddTask = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default AddTask;
