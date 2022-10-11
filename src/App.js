ec589b3c634d92c9021d67e830b8e5bccf1aa37a
James Owiti Maurice <james.owiti@student.moringaschool.com>
2022-10-11 15:01:26 +0300

    Edit readme file

--- src/App.js
+++ src/App.js
@@ -121,7 +121,7 @@ function App() {
     <div className="container App">
 
     <br /><br />
-    <h2>To Do List App (ReactJS)</h2>
+    <h2>Jaime's To Do List App</h2>
     <br /><br />
 
     {updateData && updateData ? (
